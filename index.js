74e47ef4d5b606130589243c3d7dad79da550b1e
Luis Gabriel <gabrielvazquezm30@gmail.com>
2026-03-14 14:46:35 -0600

    practic-4

--- index.js
+++ index.js
@@ -1,11 +1,53 @@
 const express = require('express');
 const app = express();
+const {pokemon} = require('./pokedex.json');
 
 app.get("/",(req, res, next) => {
-    res.status(200);
-    res.send("Bienvenido");
     
+    res.send("Bienvenido al pokedex");
+    res.status(200);
 });
-app.listen(3000, () => {
+
+app.get('/:pokemon/all',(req, res, next) => {
+    
+    res.status(200);
+    res.send(pokemon);
+});
+
+app.get('/pokemon/:name', (req, res) => {
+    const name = req.params.name;
+    for(i=0; i < pokemon.length; i++) {
+        if(pokemon[i].name ==name){
+            res.status(200).send(pokemon[i]);
+
+        }
+
+    if (pokemonFound) {
+        return res.status(200).send(pokemonFound);
+    }
+}
+    res.status(404).send("Pokemon no encontrado");
+});
+
+app.get('/pokemon/:id', (req, res) => {
+    const idParam = req.params.id;
+
+   if (isNaN(idParam)) {
+        return res.status(400).send("Error: El ID debe ser un número, no texto.");
+    }
+
+    const id = parseInt(idParam) - 1;
+
+    if (id >= 0 && id < pokemon.length) {
+        return res.status(200).send(pokemon[id]);
+    }
+    
+    res.status(404).send("Pokemon no encontrado");
+    
+})
+
+
+    
+app.listen(process.env.PORT || 3000, () => {
 console.log('Server is running ....');
 });
