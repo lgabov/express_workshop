337d64e32e38e99e1ea09c6b850eace272483bb8
Luis Gabriel <gabrielvazquezm30@gmail.com>
2026-04-18 14:14:50 -0600

    Instalando JWT

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const user = express.Router();
 const db = require('../config/database');
 
@@ -15,6 +16,26 @@ if(rows.affectedRows == 1) {
 }
 return res.status(500).json({ code: 500, message: "Error al insertar el usuario" });
 
+    }
+    return res.status(500).json({ code: 500, message: "Campos incompletos" });
+});
+
+user.post("/login", async (req, res, next) => {
+  const { user_mail, user_password } = req.body;
+  const query = `SELECT * FROM user WHERE user_mail = '${user_mail}' AND user_password = '${user_password}';`;
+  const rows = await db.query(query);
+
+  if(user_mail && user_password){
+if(rows.length == 1) {
+    const token = jwt.sign({ 
+        user_id: rows[0].user_id,
+        user_mail: rows[0].user_mail
+    }, "debugkey");
+    return res.status(200).json({ code: 200, message: token });
+}
+else {
+    return res.status(401).json({ code: 401, message: "Usuario y/o contraseña incorrectas" });
+}
     }
     return res.status(500).json({ code: 500, message: "Campos incompletos" });
 });
