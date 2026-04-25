23bb59157e24942d25e8acd3463e1da82d0a2acf
Luis Gabriel <gabrielvazquezm30@gmail.com>
2026-04-25 09:06:25 -0600

    Implementando JWT

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-
+//Dependencias
 const morgan = require('morgan');
 const express = require('express');
 const app = express();
+//Routers
 const pokemon = require('./routes/pokemon');
 const user = require('./routes/user');
+//Midlewares
+const auth = require('./middleware/auth');
+const notFound = require('./middleware/notFound');
+const index = require('./middleware/index');
 
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/",(req, res, next) => {
-   return res.status(200).json({ code: 1, message: "Bienvenido al pokedex" });
+app.get("/", index);
 
-});
-
-app.use('/pokemon', pokemon);
 app.use('/user', user);
+app.use(auth);
+app.use('/pokemon', pokemon);
 
-app.use((req, res, next) => {
-   return res.status(404).json({ code: 404, message: "url no encontrada" });
-});
+app.use(notFound);
     
 app.listen(process.env.PORT || 3000, () => {
 console.log('Server is running ....');
